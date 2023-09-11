c3b98b5305d43fa9a406c6ae6db0948a891a2795
e.kobedevon <e.kobedevon@gmail.com>
2023-09-10 21:46:03 -0500

    Remove test functions from API

--- backend/schema.ts
+++ backend/schema.ts
@@ -1,12 +1,1 @@
 import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
-
-export const users = pgTable('users', {
-	id: serial('id').primaryKey(),
-	fullName: text('full_name'),
-	phone: varchar('phone', { length: 256 })
-});
-
-export const notes = pgTable('notes', {
-	id: serial('id').primaryKey(),
-	text: varchar('text', { length: 60 })
-});
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,5 @@
 import express from 'express';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import { notes, users } from '../schema';
 import cors from 'cors';
-import { eq } from 'drizzle-orm';
 const app = express();
 
 const port = 8000;
@@ -11,60 +7,17 @@ const port = 8000;
 const corsOptions = {
 	origin: '*', // Replace with the allowed origin
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	credentials: true, // Enable cookies and credentials (if needed)
+	credentials: true, // Enable cookies and credentials
 	optionsSuccessStatus: 204 // No content response for preflight requests
 };
 
 app.use(cors(corsOptions));
-const client = postgres({
-	user: 'postgres',
-	database: 'test',
-	hostname: 'localhost',
-	port: 5433,
-	password: 'example'
-});
-const db = drizzle(client);
-const testAsync = async () => {
-	const allUsers = await db.select().from(users);
-	console.log(allUsers);
-	return allUsers;
-};
 
-app.get('/', (req, res) => {
+
+app.get('/', (_req, res) => {
 	res.send('Hello World!');
 });
 
-app.get('/names', async (req, res) => {
-	const data = await testAsync();
-	console.log(data);
-	res.json({ data });
-});
-
-app.get('/notes', async (req, res) => {
-	const data = await db.select().from(notes);
-	console.log(data);
-	res.json({ data });
-});
-
-app.post('/newNote', async (req, res) => {
-	console.log(req.query);
-	const text = req.query.text as string;
-	console.log('HERE');
-	console.log(text);
-	const data = await db.insert(notes).values({ text: text });
-	const datas = await db.select().from(notes);
-	res.status(200).json({ datas });
-});
-
-app.post('/deleteNote',async (req,res) => {
-	console.log(req.query);
-	const id:number = +req.query.id;
-	console.log(typeof(id))
-	console.log(id)
-	const deleted = await db.delete(notes).where(eq(notes.id,id))
-	.returning()
-	res.status(200).json({deleted})
-})
 
 app.listen(port, () => {
 	return console.log(`Express is listening at http://localhost:${port}`);
