1f00ff2f218a1ed517ea39217d0fa4f9c16e9fa4
e.kobedevon <e.kobedevon@gmail.com>
2023-09-10 23:47:44 -0500

    enable prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-const app = express();
+import express from 'express'
+import cors from 'cors'
+const app = express()
 
-const port = 8000;
+const port = 8000
 
 const corsOptions = {
-	origin: '*', // Replace with the allowed origin
-	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-	credentials: true, // Enable cookies and credentials
-	optionsSuccessStatus: 204 // No content response for preflight requests
-};
+    origin: '*', // Replace with the allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable cookies and credentials
+    optionsSuccessStatus: 204, // No content response for preflight requests
+}
 
-app.use(cors(corsOptions));
-
+app.use(cors(corsOptions))
 
 app.get('/', (_req, res) => {
-	res.send('Hello World!');
-});
-
+    res.send('Hello World!')
+})
 
 app.listen(port, () => {
-	return console.log(`Express is listening at http://localhost:${port}`);
-});
+    return console.log(`Express is listening at http://localhost:${port}`)
+})
