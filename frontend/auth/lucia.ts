2b1f698766ff8a4c48ffea6e789474a60ccfc8b9
Kobe Edmond <e.kobedevon@gmail.com>
2023-09-15 04:05:20 +0000

    update lucia

--- frontend/auth/lucia.ts
+++ frontend/auth/lucia.ts
@@ -1,5 +1,5 @@
 import { lucia } from 'lucia'
-import { nextjs } from 'lucia/middleware'
+import { nextjs_future } from 'lucia/middleware'
 import { postgres as postgresAdapter } from '@lucia-auth/adapter-postgresql'
 import client from '../db'
 
@@ -10,13 +10,12 @@ export const auth = lucia({
         session: 'user_session',
     }),
     env: process.env.NODE_ENV === "development" ? "DEV" : "PROD", // "PROD" if deployed to HTTPS
-    middleware: nextjs(),
+    middleware: nextjs_future(),
     getUserAttributes: (data:any) => {
         return {
             username: data.username,
         }
     },
-	csrfProtection:{allowedSubdomains:"*"}
 })
 
 export type Auth = typeof auth
